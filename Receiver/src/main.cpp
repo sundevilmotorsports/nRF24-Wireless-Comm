ee3d59dcab8c2c8d279d52da1efb77a8ee2cd46b
Kaden Greenlief <kgreenl3@asu.edu>
2024-01-13 14:36:59 -0700

    Starting message encoding decoding 🚩🆘🆘🆘🚩

--- Receiver/src/main.cpp
+++ Receiver/src/main.cpp
@@ -27,10 +27,32 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:  
   if (radio.available()) {
-    char text[32] = "";
+    //Decoding general packet
+    uint8_t text[32];
     radio.read(&text, sizeof(text));
-    Serial.println(text);
-    Serial.println("data:");
+    uint8_t ID = text[0];
+    int timestamp = text[1] << 24 | text[2] << 16 | text[3] << 8 | text[4];
+    uint8_t lap_no = text[5];
+    short avg_speed = text[6] << 8 | text[7];
+    int lat_gps = text[8] << 24 | text[9] << 16 | text[10] << 8 | text[11];
+    int lon_gps = text[12] << 24 | text[13] << 16 | text[14] << 8 | text[15];
+    short lat_g = text[16] << 8 | text[17];
+    short lon_g = text[18] << 8 | text[19];
+
+    Serial.print(ID);
+    Serial.print(",");
+    Serial.print(timestamp);
+    Serial.print(",");
+    Serial.println(lap_no);
+    Serial.print(",");
+    Serial.print(avg_speed);
+    Serial.print(",");
+    Serial.print(lat_gps);
+    Serial.print(",");
+    Serial.print(lat_g);
+    Serial.print(",");
+    Serial.println(lon_g);
   }
+  delay(20);
 }
 
