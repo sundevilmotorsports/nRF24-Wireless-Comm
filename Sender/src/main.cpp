5450e93197a6e8565d16e8e9f7b6b4539cab2468
IWumbo203 <ryansleigh2003@gmail.com>
2024-01-13 15:37:35 -0700

    i made this with my tears 💦😢😢😢😢💦

--- Sender/src/main.cpp
+++ Sender/src/main.cpp
@@ -13,17 +13,54 @@ uint8_t mailBoxes = 10; // Amount of mail boxes in system
 RF24 radio(CE,CSE);// Radio object with CE,CSN pins given
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> myCan; // initialize in can2.0 mode 
 // Function declarations:
-int count = 0;
-String str = String(count);
+int timestamp = 0;
+String str = String(timestamp);
 char arr[] = "bruh";
+uint8_t pkt[32];
+short ave_speed;
+uint8_t lap_no;
+int lat_gps;
+int lon_gps;
+short lat_g;
+short lon_g;
 
+//ID, TIMESTAMP, LAPNO, AVG_SPEED, LAT_GPS, LON_GPS, LAT_G, LON_G
 void testSend() {
-  const char text[] = "you suck";
-  radio.write(&text, sizeof(text));
-  count++;
-  radio.write(&arr, sizeof(arr));
+
+
+  ave_speed = rand() % 101;
+  lap_no = (timestamp / 240) + 1;
+  lat_gps = rand() % 500 + 1000;
+  lon_gps =  rand() % 500 + 1000;
+  lat_g = rand() % 4 + 0;
+  lon_g = rand() % 4 + 0; 
+  pkt[0] = 3;
+  pkt[1] = (timestamp & 0xFF000000) >> 24;
+  pkt[2] = (timestamp & 0x00FF0000) >> 16;
+  pkt[3] = (timestamp & 0x0000FF00) >> 8;
+  pkt[4] = timestamp & 0x000000FF;
+  pkt[5] = lap_no;
+  pkt[6] = (ave_speed & 0xFF00) >> 8;
+  pkt[7] = ave_speed & 0x00FF;
+  pkt[8] = (lat_gps & 0xFF000000) >> 24;
+  pkt[9] = (lat_gps & 0x00FF0000) >> 16;
+  pkt[10] = (lat_gps & 0x0000FF00) >> 8;
+  pkt[11] = lat_gps & 0x000000FF;
+  pkt[12] = (lon_gps & 0xFF000000) >> 24;
+  pkt[13] = (lon_gps & 0x00FF0000) >> 16;
+  pkt[14] = (lon_gps & 0x0000FF00) >> 8;
+  pkt[15] = lon_gps & 0x000000FF;
+  pkt[16] = (lat_g & 0xFF00) >> 8;
+  pkt[17] = lat_g & 0x00FF;
+  pkt[18] = (lon_g & 0xFF00) >> 8;
+  pkt[19] = lon_g & 0x00FF;
+ 
+  radio.write(&pkt, sizeof(pkt));
+  timestamp++;
+  //radio.write(&arr, sizeof(arr));
+  Serial.print(ave_speed);
   Serial.print(" ; hello ");
-  Serial.print(count);
+  Serial.println(timestamp);
 
   delay(1000);
 }
